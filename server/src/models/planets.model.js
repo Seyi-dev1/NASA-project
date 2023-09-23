f29b4d0c65a61aea67670c136c936e0137812012
Oluwaseyi Samuel <samoluwaseyi25@gmail.com>
2023-09-23 17:45:15 +0100

    adding planets to mongo db and pulling themon page load

--- server/src/models/planets.model.js
+++ server/src/models/planets.model.js
@@ -2,9 +2,7 @@ const { parse } = require("csv-parse");
 const fs = require("fs");
 const path = require("path");
 
-const results = [];
-
-const habitablePlanets = [];
+const planets = require("./planets.mongo");
 
 const isHabitablePlanet = (planet) => {
   return (
@@ -15,7 +13,7 @@ const isHabitablePlanet = (planet) => {
   );
 };
 
-const loadPlanetsData = () => {
+function loadPlanetsData() {
   return new Promise((resolve, reject) => {
     fs.createReadStream(
       path.join(__dirname, "..", "..", "data", "kepler_data.csv")
@@ -26,25 +24,49 @@ const loadPlanetsData = () => {
           columns: true,
         })
       )
-      .on("data", (data) => {
+      .on("data", async (data) => {
         if (isHabitablePlanet(data)) {
-          habitablePlanets.push(data);
+          savePlanet(data);
         }
-        results.push(data);
       })
       .on("error", (err) => {
         console.log(err);
         reject(err);
       })
-      .on("end", () => {
-        console.log(`${habitablePlanets.length} habitable planets found`);
+      .on("end", async () => {
+        const countPlanetsFound = (await getAllPlanets()).length;
+        console.log(`${countPlanetsFound} habitable planets found`);
         resolve();
       });
   });
-};
+}
 
-function getAllPlanets() {
-  return habitablePlanets;
+async function getAllPlanets() {
+  return await planets.find(
+    {},
+    {
+      _id: 0,
+      __v: 0,
+    }
+  );
+}
+
+async function savePlanet(planet) {
+  try {
+    await planets.updateOne(
+      {
+        keplerName: planet.kepler_name,
+      },
+      {
+        keplerName: planet.kepler_name,
+      },
+      {
+        upsert: true,
+      }
+    );
+  } catch (err) {
+    console.error(`Could not save planet ${err}`);
+  }
 }
 
 module.exports = {
--- server/src/server.js
+++ server/src/server.js
@@ -9,7 +9,7 @@ const { loadPlanetsData, loadPlanets } = require("./models/planets.model");
 const PORT = process.env.PORT || 8000;
 
 const MONGO_URL =
-  "mongodb+srv://samoluwaseyi25:oswagz19@nasa-cluster.5do16tv.mongodb.net/nasa?retryWrites=true&w=majority&appName=AtlasApp";
+  "mongodb+srv://samoluwaseyi25:hv3iW4liULY1DdoG@nasa-cluster.5do16tv.mongodb.net/nasa?retryWrites=true&w=majority&appName=AtlasApp";
 
 const server = http.createServer(app);
 
